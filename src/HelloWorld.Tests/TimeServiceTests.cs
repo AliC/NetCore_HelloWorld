14661be018a1ea7bd2e77881b9b890474b0978d0
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-06-12 20:20:35 +0100

    Increased test coverage for time service

--- src/HelloWorld.Tests/TimeServiceTests.cs
+++ src/HelloWorld.Tests/TimeServiceTests.cs
@@ -11,6 +11,30 @@ namespace HelloWorld.Tests
         private IFixture _fixture = new Fixture();
 
         [Fact]
+        public void Returns_Correctly_Formatted_Time_When_Minutes_Under_Ten_And_Hours_Under_Ten_And_Before_Noon()
+        {
+            var now = new DateTime(2017, 10, 14, 5, 3, 0);
+            var timeService = new TimeService(now);
+            var expectedTime = "5:03 AM";
+
+            var actualTime = timeService.Time;
+
+            Assert.Equal(expectedTime, actualTime);
+        }
+
+        [Fact]
+        public void Returns_Correctly_Formatted_Time_When_Minutes_Over_Nine_And_Hours_Over_Nine_And_After_Noon()
+        {
+            var now = new DateTime(2017, 10, 14, 22, 35, 0);
+            var timeService = new TimeService(now);
+            var expectedTime = "10:35 PM";
+
+            var actualTime = timeService.Time;
+
+            Assert.Equal(expectedTime, actualTime);
+        }
+        
+        [Fact]
         public void Returns_Correctly_Formatted_Date()
         {
             var now = new DateTime(2017, 10, 14, 17, 35, 0);
@@ -20,54 +44,6 @@ namespace HelloWorld.Tests
             var actualDate = timeService.Date;
 
             Assert.Equal(expectedDate, actualDate);
-        }
-
-        [Fact]
-        public void Returns_Correctly_Formatted_Time_When_Minutes_Under_Ten()
-        {
-            var now = new DateTime(2017, 10, 14, 17, 3, 0);
-            var timeService = new TimeService(now);
-            var expectedTime = "5:03 PM";
-
-            var actualTime = timeService.Time;
-
-            Assert.Equal(expectedTime, actualTime);
-        }
-
-        [Fact]
-        public void Returns_Correctly_Formatted_Time_When_Minutes_Over_Nine()
-        {
-            var now = new DateTime(2017, 10, 14, 17, 35, 0);
-            var timeService = new TimeService(now);
-            var expectedTime = "5:35 PM";
-
-            var actualTime = timeService.Time;
-
-            Assert.Equal(expectedTime, actualTime);
-        }
-
-        [Fact]
-        public void Returns_Correctly_Formatted_Time_When_Hours_Under_Ten()
-        {
-            var now = new DateTime(2017, 10, 14, 5, 35, 0);
-            var timeService = new TimeService(now);
-            var expectedTime = "5:35 AM";
-
-            var actualTime = timeService.Time;
-
-            Assert.Equal(expectedTime, actualTime);
-        }
-
-        [Fact]
-        public void Returns_Correctly_Formatted_Time_When_Hours_Over_Nine()
-        {
-            var now = new DateTime(2017, 10, 14, 11, 35, 0);
-            var timeService = new TimeService(now);
-            var expectedTime = "11:35 AM";
-
-            var actualTime = timeService.Time;
-
-            Assert.Equal(expectedTime, actualTime);
         }
 
         [Fact]
